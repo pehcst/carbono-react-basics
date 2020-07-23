6b0819c9e1c349ea1bae04d8a730dd1fab5c7ca8
Pedro Costa <pehcst@outlook.com.br>
2020-07-22 23:13:11 -0300

    finish front for agendamento and agendas

--- src/pages/Agendamento/index.js
+++ src/pages/Agendamento/index.js
@@ -1,7 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiTrash2 } from "react-icons/fi";
+
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
 
 function Agendamento() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="agendamento-container">
+      <header>
+        <img src={logoImg} alt="Unimed Volta Redonda" />
+        <span>Agendamento de exames</span>
+        <Link className="button" to="/agendar">
+          Realizar um agendamento
+        </Link>
+      </header>
+      <h1>Agendamentos realizados</h1>
+      <ul>
+        <li>
+          <strong>Exame</strong>
+          <p>Hemoglobina completo</p>
+          <strong>Paciente</strong>
+          <p>João Carlos Teixeira</p>
+          <strong>Data do agendamento</strong>
+          <p>01/01/2019</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#ddd" />
+          </button>
+        </li>
+        <li>
+          <strong>Exame</strong>
+          <p>Hemoglobina completo</p>
+          <strong>Paciente</strong>
+          <p>João Carlos Teixeira</p>
+          <strong>Data do agendamento</strong>
+          <p>01/01/2019</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#ddd" />
+          </button>
+        </li>
+        <li>
+          <strong>Exame</strong>
+          <p>Hemoglobina completo</p>
+          <strong>Paciente</strong>
+          <p>João Carlos Teixeira</p>
+          <strong>Data do agendamento</strong>
+          <p>01/01/2019</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#ddd" />
+          </button>
+        </li>
+        <li>
+          <strong>Exame</strong>
+          <p>Hemoglobina completo</p>
+          <strong>Paciente</strong>
+          <p>João Carlos Teixeira</p>
+          <strong>Data do agendamento</strong>
+          <p>01/01/2019</p>
+          <button type="button">
+            <FiTrash2 size={20} color="#ddd" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Agendamento;
